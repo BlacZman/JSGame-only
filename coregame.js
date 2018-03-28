df215a31e07b2e5c5a85dc182a2e3cf5b18e3ac5
BlacZman <37856140+BlacZman@users.noreply.github.com>
2018-03-28 13:46:58 +0700

    Add the frame of the work
    especially character class that we can add more of them

---
+++ coregame.js
@@ -0,0 +1,85 @@
+function Startgame() {
+	gameArea.start();
+	Player = new Character("material/Character/element1.png", 5, "Robert", 10, 600);
+}
+
+var gameArea = {
+	Box : document.createElement("canvas"),
+	start : function() {
+		var area = this.Box;
+		area.width = 1028;
+		area.height = 720;
+		area.style.border = "1px solid black";
+		this.ctx = area.getContext("2d");
+		document.body.insertBefore(area, document.body.childNodes[0]);
+		this.interval = setInterval(updateFrame, 1000/60);
+		window.addEventListener("keydown", function(e) {
+			gameArea.keys = (gameArea.keys || []);
+			gameArea.keys[e.keyCode] = (e.type == "keydown");
+		})
+		window.addEventListener("keyup", function(e) {
+			gameArea.keys = (gameArea.keys || []);
+			gameArea.keys[e.keyCode] = (e.type == "keydown");
+		})
+	},
+	clear : function() {
+		this.ctx.clearRect(0, 0, this.Box.width, this.Box.height);
+	}
+}
+class Character {
+	constructor(imgsrc, /*type, */speed, name, x, y) {
+		this.image = new Image();
+		this.image.src = imgsrc;
+		//this.type = type; //enemy, friend or neutral
+		this.jumpspeed = 15;
+		this.curspeedX = 0;
+		this.speedX = speed;
+		this.speedY = 0;
+		this.curspeedY = 0;
+		this.name = name;
+		this.posX = x;
+		this.posY = y;
+		this.gravity = 0.9;
+	}
+	update() {
+		var box = gameArea.ctx;
+		box.drawImage(this.image, this.posX, this.posY/*, this.image.width, this.image.height*/);
+	}
+	newPos() {
+		this.posX += this.curspeedX;
+		this.curspeedY += this.speedY + this.gravity;
+		this.posY += this.curspeedY;
+		this.hitObject();
+	}
+	jump() {
+		this.speedY += jumpspeed;
+	}
+	hitObject(obj) {
+		if (obj == null) {
+			var bottom = gameArea.Box.height-this.image.height;
+			var left = 0;
+			var right = gameArea.Box.width-this.image.width;
+			var top = 0;
+			if (this.posY > bottom){
+				this.posY = bottom;
+			}
+			if (this.posY < top) {
+				this.posY = top;
+			}
+			if (this.posX < left) {
+				this.posX = left;
+			}
+			if (this.posX > right) {
+				this.posX = right;
+			}
+		}
+	}
+}
+var updateFrame = function() {
+	gameArea.clear();
+	Player.curspeedX = 0;
+	if (gameArea.keys && gameArea.keys[37]) {Player.curspeedX = -Player.speedX}
+	if (gameArea.keys && gameArea.keys[39]) {Player.curspeedX = Player.speedX}
+	Player.newPos();
+	Player.update();
+}
